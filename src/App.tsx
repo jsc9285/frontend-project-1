36033b178e7be298a37f5972f139e5777fafbc7b
jsc9285 <60956884+jsc9285@users.noreply.github.com>
2023-10-26 14:07:21 +0900

    feat: first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Header from './components/Header';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+function App() {
+  return (
+    <Router>
+      <Header />
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/components/Header.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+// import "./Review.scoped.scss";
+
+const Review = () => {
+
+    return (
+        <>
+            <div style={{border: '1px solid red', height: '100px'}}>
+              header
+            </div>
+            
+        </>
+    )
+}
+
+export default Review;
